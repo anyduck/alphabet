4c792f1600de86b7a84d194d862e7a001ef93330
Snakeheart404 <polina.glazunova@yahoo.com>
2021-05-20 14:12:55 +0300

    fix

--- app/config/bot.py
+++ app/config/bot.py
@@ -5,5 +5,5 @@ import yaml
 API_TOKEN = os.getenv('API_TOKEN')
 WEB_HOST = os.getenv('WEB_HOST')
 
-with open('locales/uk.yml', 'r') as file:
+with open('locales/uk.yml', encoding='utf8') as file:
     MESSAGES = yaml.safe_load(file)
